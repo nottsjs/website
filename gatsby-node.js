1702aec321087efb07b105866ba449b925aa6585
David Wood <david.p.wood@gmail.com>
2019-11-28 07:25:10 +0000

    add sample event posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,9 +7,12 @@
 // You can delete this file if you're not using it
 
 const path = require(`path`)
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
+
   const blogPostTemplate = path.resolve(`src/templates/blog-template.jsx`)
+
   const result = await graphql(`
     {
       allMarkdownRemark(
@@ -26,11 +29,13 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       }
     }
   `)
+
   // Handle errors
   if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
     return
   }
+
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.frontmatter.path,
